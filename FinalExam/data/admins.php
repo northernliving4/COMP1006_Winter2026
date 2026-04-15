d54d6fda7b1bd742a4419df3228bad8300566c92
Matthew Zarcone <81539632+northernliving4@users.noreply.github.com>
2026-04-15 15:03:45 -0400

    basic structure done and working, hosted locally

--- FinalExam/data/admins.php
+++ FinalExam/data/admins.php
@@ -1,9 +1,17 @@
 <?php
-return [
-    [
-        "id" => 1,
-        "username" => "admin",
-        "email" => "admin@example.com",
-        "password_hash" => "$2y$10$example"
-    ]
-];
+return array (
+  0 => 
+  array (
+    'id' => 1,
+    'username' => 'matthew',
+    'email' => 'test@example.com',
+    'password_hash' => '$2y$10$V8OW/uEigdgnTrVHB8QKl.Ca49WQx3Ed.6DzcvKgLvZoBctdtCHm.',
+  ),
+  1 => 
+  array (
+    'id' => 2,
+    'username' => 'Matthew',
+    'email' => 'example@test.com',
+    'password_hash' => '$2y$10$ckHIEehQzlYKjDa5P87omuUgAzIz93s5HpyXmuAaxtE/9KgCBhji6',
+  ),
+);
--- FinalExam/data/images.php
+++ FinalExam/data/images.php
@@ -1,2 +1,21 @@
 <?php
-return [];
+return array (
+  0 => 
+  array (
+    'id' => 1,
+    'title' => 'php',
+    'file_path' => 'uploads/img_69dfe00e05e694.41535611.png',
+  ),
+  1 => 
+  array (
+    'id' => 2,
+    'title' => 'php',
+    'file_path' => 'uploads/img_69dfe056de7001.83416977.png',
+  ),
+  2 => 
+  array (
+    'id' => 3,
+    'title' => 'fff',
+    'file_path' => 'uploads/img_69dfe05e300d05.20457855.png',
+  ),
+);
