4e57533a2fcc25a2c7336af9e7f89917b9595163
Matthew Zarcone <81539632+northernliving4@users.noreply.github.com>
2026-04-15 15:08:07 -0400

    e

--- FinalExam/data/images.php
+++ FinalExam/data/images.php
@@ -1,21 +1,3 @@
 <?php
 return array (
-  0 => 
-  array (
-    'id' => 1,
-    'title' => 'php',
-    'file_path' => 'uploads/img_69dfe00e05e694.41535611.png',
-  ),
-  1 => 
-  array (
-    'id' => 2,
-    'title' => 'php',
-    'file_path' => 'uploads/img_69dfe056de7001.83416977.png',
-  ),
-  2 => 
-  array (
-    'id' => 3,
-    'title' => 'fff',
-    'file_path' => 'uploads/img_69dfe05e300d05.20457855.png',
-  ),
 );
